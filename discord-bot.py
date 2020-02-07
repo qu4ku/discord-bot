520bc05860a0d400bcc6111a0c20add6e03bd4a6
qu4ku <qu4ku@hotmail.com>
2020-02-07 18:28:33 +0100

    disc

--- discord-bot.py
+++ discord-bot.py
@@ -1,7 +1,8 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 import random
+from itertools import cycle
 
 from creds import TOKEN
 
@@ -11,7 +12,9 @@ client = commands.Bot(command_prefix='.')
 
 @client.event
 async def on_ready():
-	print('Bot is ready.')
+	await client.change_presence(status=discord.Status.idle, activity=discord.Game('Hello there.'))
+	change_status.start()
+	print('Bot is online.')
 
 
 @client.event
@@ -49,6 +52,7 @@ async def clear(ctx, amount=5):
 
 
 @client.command()
+@commands.has_permissions(manage_messages=True)
 async def kick(ctx, member : discord.Member, *, reason=None):
 	await member.kick(reason=reason)
 	await ctx.send(f'Kicked {user.mention}')
@@ -71,5 +75,45 @@ async def unban(ctx, *, member):
 			await ctx.send(f'Unbanned {user.mention}')
 			return
 
+
+# Loops / background task
+# We start a loop in on_ready
+STATUS = cycle(['This is the first status!', 'The second status here.'])
+# STATUS = cycle(['status1', 'status2'])
+@tasks.loop(seconds=10)
+async def change_status():
+	await client.change_presence(activity=discord.Game(next(STATUS)))
+
+
+# Errors
+# One way of dealing with errors / more general
+# If done this way only errors specify here will be triggered[!]
+@client.event
+async def on_command_error(ctx, error):
+	if isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send('Please pass in all required arguments.')
+
+	if isinstance(error, commands.CommandNotFound):
+		await ctx.send('Invalid command.')
+
+# Second way of dealing with errors / these are triggered first 
+@clear.error
+async def clear_error(ctx, error):
+	if isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send('Please specify an amount of messages to delete.')
+
+@client.command()
+async def clean(ctx, amount : int):
+	await ctx.channel.purge(limit=amount)
+
+
+# Custom checks
+def is_it_me(ctx):
+	return ctx.author.id == 333383838388383
+
+@client.command()
+@commands.check(is_it_me)
+async def example(ctx):
+	await ctx.send(f"Hi I'm {ctx.author}.")
 
 client.run(TOKEN)
